fc565c76c4bb4d8fc50aebc30fc68b4936133e25
rcooper <rcooper@users.noreply.github.com>
2017-01-25 11:22:29 -0500

    Update and rename config.sample.js to config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+	twitter: {
+  	consumer_key: 'oxHtMEwMEKylu7QOFhkkYFo2R',
+		consumer_secret: '9D6qzRsjRi1dK1zIMxcLO7OAlnJy7VPVEv6MjriAFHeBBBPYV9',
+		access_token: '36364896-u687lHNEq6LLMuwu2M4oRlKv1V0vaIgf6OLcfdGOQ',
+		access_token_secret: 'kEgAu6c6PneDqNVvJKgPHNJWpIF0AOEUx48JRIWnmqY1K'
+	}
+
+};
